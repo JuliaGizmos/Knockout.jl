3070c2bef0e60a515dcc973accfcfa1889899466
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-06-20 20:52:25 +0100

    switch to newer tko

--- deps/build.jl
+++ deps/build.jl
@@ -4,4 +4,4 @@ const _pkg_assets = joinpath(_pkg_root,"assets")
 
 !isdir(_pkg_assets) && mkdir(_pkg_assets)
 
-download("https://unpkg.com/knockout", joinpath(_pkg_assets, "knockout.js"))
+download("https://unpkg.com/tko@4.0.0-alpha4a/dist/tko.js", joinpath(_pkg_assets, "knockout.js"))
