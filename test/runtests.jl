9c74370a92007c0749b97b55f776eb9541eb4d40
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-06-22 18:38:58 +0100

    add sleep

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,7 +15,7 @@ n = (knockout(t, ["options" => s]));
 
 w = Window(Blink.@d(:show => false)); sleep(5.0)
 
-body!(w, n)
+body!(w, n); sleep(1.0)
 
 @test Blink.@js w document.querySelector("#myselect").children.length == 3
 @test Blink.@js w document.querySelector("#myselect").children[0].value == "a"
