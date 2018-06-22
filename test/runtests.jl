ce83b7138990d231efa55e6e7563fec4ba47f493
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-06-22 18:31:21 +0100

    fix tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,34 +5,28 @@ else
     using Test
 end
 
+cleanup = !AtomShell.isinstalled()
+cleanup && AtomShell.install()
+
 # write your own tests here
 s = Observable(["a", "b", "c"])
-t = Node(:select, attributes = Dict("data-bind" => "options : options"));
+t = Node(:select, attributes = Dict("data-bind" => "options : options", "id" => "myselect"));
 n = (knockout(t, ["options" => s]));
-cleanup = !AtomShell.isinstalled()
-
-cleanup && AtomShell.install()
 
 w = Window(Blink.@d(:show => false)); sleep(5.0)
 
 body!(w, n)
 
-str = "<select data-bind=\"options : options\">" *
-    "<option value=\"a\">a</option>" *
-    "<option value=\"b\">b</option>" *
-    "<option value=\"c\">c</option>" *
-    "</select></div>"
-blink_body = Blink.@js w document.querySelector("body").innerHTML
-@test contains(blink_body, str)
+@test Blink.@js w document.querySelector("#myselect").children.length == 3
+@test Blink.@js w document.querySelector("#myselect").children[0].value == "a"
+@test Blink.@js w document.querySelector("#myselect").children[1].value == "b"
+@test Blink.@js w document.querySelector("#myselect").children[2].value == "c"
 
 s[] = ["c", "d"]
 sleep(1.0)
-str = "<select data-bind=\"options : options\">" *
-    "<option value=\"c\">c</option>" *
-    "<option value=\"d\">d</option>" *
-    "</select></div>"
 
-blink_body = Blink.@js w document.querySelector("body").innerHTML
-@test contains(blink_body, str)
+@test Blink.@js w document.querySelector("#myselect").children.length == 2
+@test Blink.@js w document.querySelector("#myselect").children[0].value == "c"
+@test Blink.@js w document.querySelector("#myselect").children[1].value == "d"
 
 cleanup && AtomShell.uninstall()
