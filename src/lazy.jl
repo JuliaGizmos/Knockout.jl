6567c81121474749e2aae4f1da34741b1b8b2f07
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-06-28 15:16:00 +0100

    fix initialization

--- src/lazy.jl
+++ src/lazy.jl
@@ -3,7 +3,7 @@ struct LazyPair{T}
     second::Observable{Any}
     f
     g
-    function LazyPair(first::Observable{T}, second::Observable, f, g) where {T}
+    function LazyPair(first::Observable{T}, second::Observable; f = identity, g = identity) where {T}
         on(first) do val
             fval = f(val)
             (second[] == fval) || Observables.setexcludinghandlers(second, fval, x -> x !== g)
@@ -16,8 +16,8 @@ struct LazyPair{T}
     end
 end
 
-LazyPair(first::Observable, second = Observable{Any}(first[]); f = identity, g = identity) =
-    LazyPair(first, second, f, g)
+LazyPair(first::Observable; f = identity, g = identity) =
+    LazyPair(first, Observable{Any}(f(first[])); f = f, g = g)
 
 _get(x) = x
 _get(x::Observable) = x[]
