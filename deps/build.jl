e71b114e218316cff21240e05069200d75d55900
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2019-03-29 16:28:46 +0000

    fix link

--- deps/build.jl
+++ deps/build.jl
@@ -4,4 +4,4 @@ const _pkg_assets = joinpath(_pkg_root,"assets")
 
 !isdir(_pkg_assets) && mkdir(_pkg_assets)
 
-download("https://unpkg.com/tko@4.0.0-alpha5h/dist/tko.min.js", joinpath(_pkg_assets, "knockout.js"))
+download("https://cdn.jsdelivr.net/npm/@tko/build.reference@4.0.0-alpha8/dist/build.reference.min.js", joinpath(_pkg_assets, "knockout.js"))
